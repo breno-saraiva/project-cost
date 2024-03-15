1d3973f202dcd2ee123d216561da7033acf14f12
breno-saraiva <breno.saraiva@livsaude.com.br>
2024-03-15 12:37:38 -0300

    fix project page

--- teste/src/pages/Projetc/index.tsx
+++ teste/src/pages/Projetc/index.tsx
@@ -1,13 +1,18 @@
-import { Message } from "../../components/Message";
 import Navbar from "../../components/navbar";
 import Footer from "../../components/Footer";
+import { Linkbutton } from "../Home/components/LinkButton";
 
 function Project() {
   return (
     <div>
       <Navbar />
-      <h1 className="mb-4 text-5xl font-sans font-bold">Meus Projetos</h1>
-      <Message msg="Alguma mensagem" />
+      <div className="flex justify-between items-center px-16 py-8">
+        <h1 className="mb-4 text-5xl font-sans font-bold">Meus Projetos</h1>
+        <Linkbutton to="/newProject" text="Criar projeto" />
+      </div>
+      <div className="h-full px-16">
+        <p>Projetos</p>
+      </div>
       <Footer />
     </div>
   );
